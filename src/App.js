fadc3b91f1b92a7e49f940f87f09a0d8b11a9bab
mohit <mohitsolanki06123@gmail.com>
2023-03-19 18:36:48 +0530

    second-web deployed

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/draw/:id" element={<ImageEditor />} />
+        <Route path="/videoplayer/" element={<Home />} />
+        <Route path="/videoplayer/draw/:id" element={<ImageEditor />} />
       </Routes>
 
       
